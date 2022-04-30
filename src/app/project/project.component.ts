525eb81e087569045a2b611cca6ac3a83d9e9abb
Nicholas-Coorevits <71607944+Nicholas-Coorevits@users.noreply.github.com>
2022-04-30 21:17:57 +0200

    mobile-ready ongeveer

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -10,6 +10,7 @@ export class ProjectComponent implements OnInit {
   ShowProj1:boolean=true
   ShowProj2:boolean=false
   ShowProj3:boolean=false
+  ShowProj4:boolean=false
   constructor() { }
 
   ngOnInit(): void {
@@ -17,6 +18,7 @@ export class ProjectComponent implements OnInit {
   ToggleTag1() {
     this.ShowProj2=false
     this.ShowProj3=false
+    this.ShowProj4=false
     if (!this.ShowProj1) {
       this.ShowProj1=!this.ShowProj1
     }
@@ -25,8 +27,27 @@ export class ProjectComponent implements OnInit {
   ToggleTag2() {
     this.ShowProj1=false
     this.ShowProj3=false
+    this.ShowProj4=false
     if(!this.ShowProj2) {
       this.ShowProj2=!this.ShowProj2
+    }
+  }
+
+  ToggleTag3() {
+    this.ShowProj2=false
+    this.ShowProj1=false
+    this.ShowProj4=false
+    if(!this.ShowProj3) {
+      this.ShowProj3=!this.ShowProj3
+    }
+  }
+
+  ToggleTag4() {
+    this.ShowProj2=false
+    this.ShowProj1=false
+    this.ShowProj3=false
+    if(!this.ShowProj4) {
+      this.ShowProj4=!this.ShowProj4
     }
   }
 
